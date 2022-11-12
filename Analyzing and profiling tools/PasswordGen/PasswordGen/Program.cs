d69e0bdc346b5fe9bbff6d46bdebf47c7dae556e
VSVENDOR\3plvanishvili <Levani_Vanishvili@epam.com>
2022-11-13 01:19:22 +0400

    changed password gen

--- Analyzing and profiling tools/PasswordGen/PasswordGen/Program.cs
+++ Analyzing and profiling tools/PasswordGen/PasswordGen/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -8,21 +9,25 @@ class Program
     {
         var wt = new Stopwatch();
         byte[] salt = new byte[16];
-        wt.Start();
-        var pass1 = GeneratePasswordHashUsingSaltNotOptimized("password", salt);
-        Console.WriteLine($"password generated: {pass1}");
-        Console.WriteLine($"GeneratePasswordHashUsingSaltNotOptimized - Milliseconds: {wt.ElapsedMilliseconds}");
+        var iterate = 10000;    
 
-        wt.Restart();
-        var pass2 = GeneratePasswordHashUsingSaltOptimized("password", salt);
-        Console.WriteLine($"password generated: {pass2}");
+        wt.Start();
+        var pass1 = GeneratePasswordHashUsingSaltOptimized("password", salt, iterate);
+        Console.WriteLine($"password generated: {pass1}");
         Console.WriteLine($"GeneratePasswordHashUsingSaltOptimized - Milliseconds: {wt.ElapsedMilliseconds}");
+        wt.Stop();
+
+        wt.Start();
+        var pass2 = GeneratePasswordHashUsingSaltNotOptimized("password", salt, iterate);
+        Console.WriteLine($"password generated: {pass2}");
+        Console.WriteLine($"GeneratePasswordHashUsingSaltNotOptimized - Milliseconds: {wt.ElapsedMilliseconds}");
+        wt.Stop();
     }
 
-    public static string GeneratePasswordHashUsingSaltNotOptimized(string passwordText, byte[] salt)
+    public static string GeneratePasswordHashUsingSaltNotOptimized(string passwordText, byte[] salt, int iterate)
     {
-        var iterate = 10000;
         var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+        
         byte[] hash = pbkdf2.GetBytes(20);
 
         byte[] hashBytes = new byte[36];
@@ -35,19 +40,15 @@ class Program
 
     }
 
-    public static string GeneratePasswordHashUsingSaltOptimized(string passwordText, byte[] salt)
+    public static string GeneratePasswordHashUsingSaltOptimized(string passwordText, byte[] salt, int iterate)
     {
-
-        var iterate = 10000;
-        var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate, HashAlgorithmName.SHA1);
-        byte[] hash = pbkdf2.GetBytes(20);
-
+        var keyDerHash = KeyDerivation.Pbkdf2(passwordText, salt, KeyDerivationPrf.HMACSHA1, iterate, 20);
+        
         byte[] hashBytes = new byte[36];
-        Array.Copy(salt, 0, hashBytes, 0, 16);
-        Array.Copy(hash, 0, hashBytes, 16, 20);
+        salt.CopyTo(hashBytes, 0);
+        keyDerHash.CopyTo(hashBytes, 16);
 
         var passwordHash = Convert.ToBase64String(hashBytes);
-        pbkdf2.Dispose();
 
         return passwordHash;
 
